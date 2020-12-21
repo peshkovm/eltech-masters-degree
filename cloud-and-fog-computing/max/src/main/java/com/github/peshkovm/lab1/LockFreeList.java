27bfead332aeafe7cf6bb2b0f7c75ecdb9db0ce2
Maksim Peshkov <peshmaxan@gmail.com>
2020-12-21 22:31:40 +0300

    Add LockFreeList

---
+++ cloud-and-fog-computing/max/src/main/java/com/github/peshkovm/lab1/LockFreeList.java
@@ -0,0 +1,19 @@
+package com.github.peshkovm.lab1;
+
+public class LockFreeList implements Set<Integer>{
+
+  @Override
+  public boolean add(Integer item) {
+    return false;
+  }
+
+  @Override
+  public boolean remove(Integer item) {
+    return false;
+  }
+
+  @Override
+  public boolean contains(Integer item) {
+    return false;
+  }
+}
